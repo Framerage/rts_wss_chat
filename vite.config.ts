dd39c6354da4c3e59ceb4286c3b1aa7eeb09b217
framerage <officialigonin@mail.ru>
2025-08-29 16:52:36 +0400

    add chat window

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,31 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),],
   server: {
     open: "/",
   },
+  //   resolve: {
+  //   alias: {
+  //     assets: "/src/assets",
+  //     api: "/src/api",
+  //     components: "/src/components",
+  //     hooks: "/src/hooks",
+  //     pages: "/src/pages",
+  //     store: "/src/store",
+  //     types: "/src/types",
+  //     utils: "/src/utils",
+  //   },
+  // },
+  base: "/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+
 });
