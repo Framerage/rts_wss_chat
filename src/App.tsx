ecc48a790f8e390ec5997e685e55d1bed63a9941
Artem Igonin <officialigonin@mail.ru>
2025-08-31 23:55:08 +0400

    add ws api and edit chat logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import styles from './styles.module.scss'
+import { useCallback, useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import styles from "./styles.module.scss";
 
-export const App=()=>{
+export const App = () => {
+  const [msgs, setMsgs] = useState<
+    { value: string; timeStamp: any; client: string }[]
+  >([]);
+  const [inputValue, setInputValue] = useState<string>();
+  const [socketState, setSocketState] = useState<WebSocket>();
+
+  const userId = useRef<number | null>(null);
+
+  const chatRef = useRef<HTMLUListElement | null>(null);
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    setSocketState(socket);
+
+    socket.onopen = (e) => {
+      console.log("connection ", e);
+      const clientId = new Date().getMilliseconds();
+      userId.current = clientId;
+    };
+
+    socket.onmessage = (e) => {
+      console.log(e, " msg event");
+      const msgData = String(e.data);
+      const msg = msgData.split("//c:")[0];
+      const client = msgData.split("//c:")[1];
+      setMsgs((prev) => [
+        ...prev,
+        {
+          value: msg,
+          client,
+          timeStamp: new Date().toLocaleTimeString(),
+        },
+      ]);
+    };
+
+    socket.onclose = (e) => {
+      console.log("closed ", e);
+    };
+
+    socket.onerror = (err) => {
+      console.log("socket error ", err);
+    };
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  const onSendMsg = useCallback(() => {
+    if (!inputValue) return;
+    if (socketState && socketState.readyState === WebSocket.OPEN) {
+      socketState.send(
+        `${inputValue.replaceAll(/^\n+|\n|\r+$/g, "")}//c:${userId.current}`
+      );
+      setInputValue("");
+    }
+  }, [inputValue, socketState, userId]);
+  console.log(msgs, " msgs", socketState);
+
+  useEffect(() => {
+    msgs.length !== 0 &&
+      chatRef?.current?.scrollTo(0, chatRef?.current?.scrollHeight);
+  }, [chatRef, msgs]);
   return (
     <main className={styles.contentWrap}>
       <section className={styles.chatContainer}>
         <div className={styles.chatHeader}>Room</div>
-        <ul className={styles.chatWindow}>
-          <li>msg 1</li>
-          <li>msg 12</li>
-          <li>msg 13</li>
+        <ul className={styles.chatWindow} ref={chatRef}>
+          {msgs.map((msg) => {
+            return (
+              <li
+                key={msg.timeStamp}
+                className={`${styles.chatMsg} ${
+                  msg.client === String(userId.current) && styles.personMsg
+                }`}
+              >
+                <p>{msg.value}</p>
+                <p>{msg.timeStamp.slice(0, -3)}</p>
+                <p>{msg.client}</p>
+              </li>
+            );
+          })}
         </ul>
         <div className={styles.chatFooter}>
-          <textarea  />
-          <button>Send</button>
+          <textarea
+            onChange={(e) => setInputValue(e.currentTarget.value)}
+            value={inputValue}
+          />
+          <button onClick={onSendMsg}>Send</button>
         </div>
       </section>
     </main>
-  )
-}
-
+  );
+};
